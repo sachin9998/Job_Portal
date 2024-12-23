d9087cdcf833e75fdeb7e52f08404d0a289cd29b
Sachin Alam <sachinalam4216@gmail.com>
2024-12-24 00:25:13 +0530

    route added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,28 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 8000;
+
+app.get("/home", (req, res) => {
+  return res.status(200).json({
+    message: "I am coming from backend",
+    sucess: true,
+  });
+});
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: "http//localhost:5173",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.listen(port, (req, res) => {
   console.log("Server is listening on Port: ", port);
